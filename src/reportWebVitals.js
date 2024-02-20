26e4258c3feadf450cfb627e8324dac3e0bc670e
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:22:55 -0600

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,18 @@
+// Function to measure web vitals and report the results
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+  // Check if the onPerfEntry callback function is provided and is a function
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    // Asynchronously import the 'web-vitals' module
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      // Call each web vitals function with the provided callback function
+      getCLS(onPerfEntry); // Cumulative Layout Shift
+      getFID(onPerfEntry); // First Input Delay
+      getFCP(onPerfEntry); // First Contentful Paint
+      getLCP(onPerfEntry); // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
     });
   }
 };
 
+// Export the reportWebVitals function
 export default reportWebVitals;
