4b6c111cb96d36f51ba44d211cbb7bcbedbd7d19
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:22:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+// Import necessary modules
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
+// Import CSS file for styling
 import './index.css';
+
+// Import the root component of the application
 import App from './App';
+
+// Import function for measuring web vitals
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Create a React root and render the application
+// The React.StrictMode component helps identify potential issues in the application
+// It activates additional checks and warnings for features such as legacy string ref
+// This is useful for identifying and fixing unsafe lifecycle methods and side effects
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, you can use reportWebVitals
+// Pass a function to log results (for example: reportWebVitals(console.log))
+// Or send data to an analytics endpoint
+// Learn more about measuring web vitals: https://bit.ly/CRA-vitals
 reportWebVitals();
